962f8e7001a105142b009d019c06818596045a57
vitorys <vitor.ysh@gmail.com>
2017-04-24 20:58:36 -0300

    Interface melhorada. Tema bootstrap alterado

--- SAAAE/app/controllers/application_controller.rb
+++ SAAAE/app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+
+	helper_method :foundPessoa
+
+	def foundPessoa(id)
+		@pessoas = Pessoa.all
+		@pessoas.each do |pessoa|
+		@teste
+			if pessoa.id == id
+				@teste = pessoa
+			end
+		end
+		@teste
+	end
+
+	helper_method :foundAluno
+
+	def foundAluno(id)
+		@alunos = Aluno.all
+		@alunos.each do |aluno|
+			if aluno.id == id
+				@return = aluno
+			end
+		end
+		@return
+	end
 end
--- SAAAE/app/models/funcionario.rb
+++ SAAAE/app/models/funcionario.rb
@@ -1,3 +1,3 @@
 class Funcionario < ApplicationRecord
-  belongs_to :pessoa
+  belongs_to :pessoa, dependent: :destroy
 end
