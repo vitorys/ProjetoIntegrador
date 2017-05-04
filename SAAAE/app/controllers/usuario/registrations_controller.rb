79347b0a65e03490a7213a4bdc5400c16448cee3
vitorys <vitor.ysh@gmail.com>
2017-05-04 16:32:31 -0300

    Navbar modificada para autenticacao

---
+++ SAAAE/app/controllers/usuario/registrations_controller.rb
@@ -0,0 +1,70 @@
+class Usuario::RegistrationsController < Devise::RegistrationsController
+
+  before_filter :configure_permitted_parameters
+
+  # GET /resource/sign_up
+   def new
+    build_resource({})
+    resource.build_profile
+    respond_with self.resource
+    end
+
+    protected
+
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u|
+      u.permit(:email, :password, :password_confirmation, :pessoa_attributes => [:pessoa_nome, :pessoa_endereco, :pessoa_cidade, :pessoa_estado,:pessoa_cep, :pessoa_email])
+    }
+  end
+
+  # POST /resource
+  # def create
+  #   super
+  # end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+end
