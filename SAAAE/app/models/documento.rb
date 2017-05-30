05eff19e595fb7b787229dfc342b8365e197dd8e
RenanKodama <renan_kdm_rdg@hotmail.com>
2017-05-29 22:45:03 -0300

    Correção do verificador de RG para no min 8 dig

--- SAAAE/app/models/documento.rb
+++ SAAAE/app/models/documento.rb
@@ -3,5 +3,5 @@ class Documento < ApplicationRecord
   	validates_numericality_of :doc_cpf, message: 'possui um caracter não válido'
   	validates_uniqueness_of :doc_cpf, message: 'já cadastrado'
   	validates_cpf_format_of :doc_cpf, message: 'possui um formato inválido'
-  	validates_length_of :doc_rg, :minimum=>9, message: 'não possui um tamanho mínimo.'
+  	validates_length_of :doc_rg, :minimum=>8, message: 'não possui um tamanho mínimo.'
 end
