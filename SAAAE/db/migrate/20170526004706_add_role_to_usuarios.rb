e95fc0de5ff96e551a64bef0fd8d9b5a8d0285a2
vitorys <vitor.ysh@gmail.com>
2017-05-30 16:48:00 -0300

    Usuario cadastrado como admin por default

--- SAAAE/db/migrate/20170526004706_add_role_to_usuarios.rb
+++ SAAAE/db/migrate/20170526004706_add_role_to_usuarios.rb
@@ -1,5 +1,5 @@
 class AddRoleToUsuarios < ActiveRecord::Migration[5.0]
   def change
-  	add_column :users, :role, :integer, :default => 0
+  	add_column :users, :role, :integer, :default => 99
   end
 end
