e42c3de5b028b72b2a41e0ff38eafaface11ddf2
vitor ys <vitor.ysh@gmail.com>
2017-08-24 01:26:06 -0300

    Recuperação de senha implementado

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,15 +21,15 @@ Rails.application.configure do
       'Cache-Control' => 'public, max-age=172800'
     }
   else
-    config.action_controller.perform_caching = false
+    config.action_controller.perform_caching = true
 
     config.cache_store = :null_store
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -53,9 +53,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 #Devise Config
 config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
+config.action_mailer.perform_deliveries = true
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
-
+config.action_mailer.smtp_settings = {
+     :address              => "smtp.gmail.com",
+     :port                 => 587,
+     :domain               => 'localhost',
+     :user_name            => 'email@exemplo.com',
+     :password             => 'senha',
+     :authentication       => :plain,
+     :enable_starttls_auto => true  }
 
 end
